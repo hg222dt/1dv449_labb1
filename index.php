80fa1a4282ceb529f05a752ed0c6e25270ff1f7a
hg222dt <henrik.gudmundsson@gmail.com>
2014-11-12 12:32:06 +0100

    Hämtar nu även ut kurskoder och url:er till kurspl

--- index.php
+++ index.php
@@ -2,36 +2,97 @@
 
     $data = curl_get_request("https://coursepress.lnu.se/kurser/");
 
-    $dom = new DomDocument();
+	$courseUrlsData = domItAndExtract($data, "//ul[@id='blogs-list']/li/div/div[@class='item-title']/a");    
 
-    if($dom->loadHTML($data)) {
-    	$xpath = new DOMXPath($dom);
+	$courseUrls = array();
 
-    	$dom_node_list = $xpath->query("//ul[@id='blogs-list']/li/div/div[@class='item-title']/a");
+	foreach($courseUrlsData as $n) {
+		array_push($courseUrls, $n->getAttribute("href"));
+	}
+
+	$courseData;
+
+	$courseDataArr = array();
 
 
 
-    	$temp_dom = new DOMDocument();
+	foreach ($courseUrls as $url) {
 
-    	$storeArray = array();
+		$courseData = curl_get_request($url);
 
-		foreach($dom_node_list as $n) {
+		array_push($courseDataArr, $courseData);
 
-			$temp_dom->appendChild($temp_dom->importNode($n,true));
-			array_push($storeArray, $n->getAttribute("href"));
+	}
 
+
+	//Kollar efter kursnamnen på varje sida
+	$courseNames = getCourseData($courseDataArr, "//div[@id='header-wrapper']/h1/a", false);
+	//var_dump($courseNames);
+	
+	//Kollar efter kurskod på varje sida
+	$courseCodes = getCourseData($courseDataArr, "//div[@id='header-wrapper']/ul/li[position()=3]/a", false);
+
+	//Hämta url:er för kursplaner
+	$coursPlanUrls = getCourseData($courseDataArr, "//ul[@id='menu-main-nav-menu']/li/ul/li[a='Kursplan']/a", true);
+
+	//$courseDescriptions = getCourseData($courseData, "//section[@id='content']/", false);
+
+	var_dump($coursPlanUrls);
+
+
+	function getCourseData($courseDataArr, $query, $getHrefAttr) {
+
+		$newTargetElements = array();
+
+		foreach ($courseDataArr as $cData) {
+
+			libxml_use_internal_errors(true);
+
+			$targetElements = domItAndExtract($cData, $query);
+			
+			libxml_use_internal_errors(false);
+
+			$temp_dom = new DOMDocument();
+		
+
+			foreach ($targetElements as $element) {
+				//$temp_dom->appendChild($temp_dom->importNode($element,true));
+				//var_dump($element);
+
+				$newdoc = new DOMDocument();
+			    $cloned = $element->cloneNode(TRUE);
+
+			    $newdoc->appendChild($newdoc->importNode($cloned,TRUE));
+			    //echo $newdoc->saveHTML();
+
+				if($getHrefAttr) {
+					foreach($targetElements as $n) {
+						array_push($newTargetElements, $n->getAttribute("href"));
+					}
+				} else {
+					array_push($newTargetElements, $cloned->nodeValue);
+				}	
+			}
 		}
 
-		//print_r($temp_dom->saveHTML());
+		return $newTargetElements;
+	}
 
-		var_dump($storeArray);
+    function domItAndExtract($data, $query) {
+    	$dom = new DomDocument();
 
-    	//var_dump($items);
+	    if($dom->loadHTML($data)) {
 
-    } else {
-    	die("Fel vid DOM-inläsning.");
-    }
+	    	$xpath = new DOMXPath($dom);
 
+	    	$dom_node_list = $xpath->query($query);
+
+	    	return $dom_node_list;
+
+	    } else {
+	    	die("Fel vid DOM-inläsning.");
+	    }
+	}
 
     
     function curl_get_request($url) {
@@ -44,7 +105,7 @@
         $data = curl_exec($ch);
         curl_close($ch);
         
-//        var_dump($data);
+        //var_dump($data);
 
         return $data;
     }
